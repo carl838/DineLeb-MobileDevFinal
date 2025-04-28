155c20b18afe1a7942b9217e94deb7d48f0885ea
carl838 <carl.bassous@net.usj.edu.lb>
2025-04-28 21:17:07 +0300

    Delete server directory

--- server/firebase.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./dinelab-firebase-adminsdk-fbsvc-f86a380dba.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
